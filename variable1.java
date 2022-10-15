c1a813a5bd4415aef3f5389ecec277237a60bb29
Hanzala Arshad <115257402+HanzalaArshad@users.noreply.github.com>
2022-10-15 12:35:11 +0500

    Update variable1.java

--- variable1.java
+++ variable1.java
@@ -5,15 +5,15 @@ class variable{
         System.out.println("the number is integer"+a);
         System.out.println("the number is integer"+b);
         double c=9.998;
-        float d=0.6;
+        double  d=0.6;
 
 System.out.println("the number is double data type"+c);
-System.out.println("the number is float data type" +df);
+System.out.println("the number is float data type" +d);
 
 char e='a';
 char f='b';
-System.out.println("it is a charcater "+'e');
-System.out.println(it is a character "+'f');
+System.out.println('e');
+System.out.println('f');
 
 
 
