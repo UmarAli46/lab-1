323d9248504dca6d54b317fcec4926d58ea05318
Hanzala Arshad <115257402+HanzalaArshad@users.noreply.github.com>
2022-10-15 12:29:06 +0500

    Update variable1.java

--- variable1.java
+++ variable1.java
@@ -2,20 +2,18 @@ class variable{
     public static void main(String[] args) {
         int a=5;
         int b=6;
-        System.out.println(a);
-        System.out.println(b);
-        double c=9.8;
-        double d=0.6;
+        System.out.println("the number is integer"+a);
+        System.out.println("the number is integer"+b);
+        double c=9.998;
+        float d=0.6;
 
-System.out.println(c);
-System.out.println(d);
+System.out.println("the number is double data type"+c);
+System.out.println("the number is float data type" +df);
 
 char e='a';
 char f='b';
-System.out.println('e');
-System.out.println('f');
-String t="java is love";
-System.out.println("t");
+System.out.println("it is a charcater "+'e');
+System.out.println(it is a character "+'f');
 
 
 
