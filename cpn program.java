a515da729fc480e5468d494de2f4766a3bedc49a
Hanzala Arshad <115257402+HanzalaArshad@users.noreply.github.com>
2022-10-15 12:23:26 +0500

    Update cpn program.java

--- cpn program.java
+++ cpn program.java
@@ -1,20 +1,16 @@
 import java.util.*;
  class cpn program {
-    public static void main(String[] args) throws Exception {
-        double a;
-        double b;
+    public static void main(String[] args) {
+        double a,b,cpn;
         int c;
-        cpn;
-        System.out.println("enter matric percentage ");
         Scanner obj=new Scanner(System.in);
+        System.out.println("enter matriculation percentage ");
         a=obj.nextInt();
         System.out.println("enter intermediate percentage ");
-        Scanner obj=new Scanner(System.in);
         b=obj.nextInt();
         System.out.println("enter test marks ");
-        Scanner obj=new Scanner(System.in);
         c=obj.nextInt();
-        cpn=(a/100)*10+(b/100)*30+(c\100)*60;
+        cpn=(a*0.10)(b*0.30)+(c*0.60);
         System.out.print("the cpn is"+cpn);
     }
 }
